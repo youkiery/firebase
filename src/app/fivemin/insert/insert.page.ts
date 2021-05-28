ccc83051aa29524de686f91a86e81986837459b9
youkiery <youkiery@gmail.com>
2021-05-28 13:40:33 +0700

    trang thêm

---
+++ src/app/fivemin/insert/insert.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert',
+  templateUrl: './insert.page.html',
+  styleUrls: ['./insert.page.scss'],
+})
+export class InsertPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
